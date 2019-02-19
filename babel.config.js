ccddc73e5eff3183a963297fd9017d186b5db856
Edmund Hung <edmund@edstud.io>
2019-02-19 20:07:51 +0800

    Manage build process on root level

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,18 @@ const pkg = require('./package.json');
 
 module.exports = {
   babelrcRoots: pkg.workspaces,
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: ['ie >= 11'],
+        },
+        exclude: ['transform-async-to-generator', 'transform-regenerator'],
+        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+        loose: true,
+      },
+    ],
+  ],
 };
